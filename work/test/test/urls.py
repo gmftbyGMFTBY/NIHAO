1a4699dfe4f0438f42a53242b26232ed01018701
GMFTBY <18811371908@163.com>
2018-05-18 21:49:55 +0800

    fuck

--- work/test/test/urls.py
+++ work/test/test/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from app01 import views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'BBS.views.home', name='home'),
+    # url(r'^$', 'test.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^add/', views.add, name='add'),
 ]
